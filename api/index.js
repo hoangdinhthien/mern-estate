b046ade46cc1845d3b6b12894da44823dc802e8d
hoangdinhthien <dinhthienhoang0408@gmail.com>
2024-07-09 15:37:07 +0700

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import { error } from 'console';
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log(`Connected to DB!`);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
 const app = express();
 
