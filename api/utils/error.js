b53bed60d5843f6319908ddb031c54e846a9242d
hoangdinhthien <dinhthienhoang0408@gmail.com>
2024-07-10 16:32:56 +0700

    create a middleware and a function to handle possible errors

---
+++ api/utils/error.js
@@ -0,0 +1,7 @@
+// function to handle possible errors
+export const errorHandler = (statusCode, message) => {
+  const error = new Error();
+  error.statusCode = statusCode;
+  error.message = message;
+  return error;
+};
