2408f72e6d5be4100a4ad9e1530699257d503bd8
hoangdinhthien <dinhthienhoang0408@gmail.com>
2024-07-15 17:05:25 +0700

    create update user API route

--- api/controllers/user.controler.js
+++ api/controllers/user.controler.js
@@ -1,5 +1,41 @@
+import User from '../models/user.model.js';
+import { errorHandler } from '../utils/error.js';
+import bcryptjs from 'bcryptjs';
+
 export const test = (req, res) => {
   res.json({
     message: 'api route is working !', // => return an object with msg
   });
+};
+
+export const updateUser = async (req, res, next) => {
+  // check if the person is authenticated or not
+  // use the cookie to verify the user
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, 'You can only update your own account!'));
+
+  try {
+    if (req.body.password) {
+      req.body.password = bcryptjs.hashSync(req.body.password, 10);
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          avatar: req.body.avatar,
+        },
+      },
+      { new: true }
+    );
+
+    const { password, ...rest } = updatedUser._doc;
+
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
 };
